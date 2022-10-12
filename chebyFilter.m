8b6a2a821d7224e233b3a8570cfbd6c6cb0ef703
Robin Dymér <robin.dymer@hotmail.com>
2022-10-12 20:37:22 +0200

    FFT

--- chebyFilter.m
+++ chebyFilter.m
@@ -1,31 +1,35 @@
 n = 10;
+ripple = 3;
 fc = 8000;
+fs = 24000; % sample freq.
 
 % Elliptic filter - steep roll-off, ripple in pass- and stopband
+% But we're satisfied with chebyshev I
 [z, p, k] = cheby1(n, ripple, 2*pi*fc, 'low', 's');
 
 % Plot magnitude and freq.
 [a, b] = cheby1(n, ripple, 2*pi*fc, 'low', 's');
-figure('Name', 'Magnitude and frequency of cheby');
+figure('Name', 'Magnitude and frequency of chebyshev I');
 freqs(a,b)
 
 % continous time
-fs = 24000;
-t_resolution = 100; % Make it CT
+t_resolution = 10; % Make it CT
 
-t_end = 0.2;
+t_end = 0.002;
 t = 0:1/(t_resolution*fs):t_end; % 0.2 förut
 
 ct_signal_wanted = sin(6000*2*pi.*t);
 ct_signal_disturbance = sin(14000*2*pi.*t);
 ct_signal_in = ct_signal_wanted + ct_signal_disturbance;
 figure('Name', 'Input signal');
-% plot(t, ct_signal_in)
-xlim([0, t_end])
+plot(t, ct_signal_in)
+% xlim([0, t_end])
 
 % Filter -> LTI system
 ct_filter = zpk(z, p, k);
 
+% Simulate output response of filter
+figure('Name', 'Output, wanted and disturbance');
 hold on
 grid on
 output_signal = lsim(ct_filter, ct_signal_in, t);
@@ -34,7 +38,13 @@ plot(t, ct_signal_wanted, 'g')
 plot(t, ct_signal_disturbance, 'r')
 hold off
 
+% Sampling
 sampled_output = output_signal(1:1/fs:end);
 t_sample = t(1:1/fs:end);
-figure('Name', 'Bruh')
+figure('Name', 'Sampled output signal')
 plot(t_sample, sampled_output)
+
+% FFT
+Y = fft(sampled_output);
+figure('Name', 'FFT')
+plot(t_sample, Y)
